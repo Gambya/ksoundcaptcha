6bafa31da198471ddd64e3c8298e44a8af1928c6
Roberto Lopes Ramos <roberto.ramos@capitallogin.local>
2018-09-05 15:03:39 -0300

    add parameter language

--- controllers/sound_captcha.py
+++ controllers/sound_captcha.py
@@ -19,6 +19,7 @@ class SoundCaptcha(Resource):
         if not path.isdir(target):
             mkdir(target)
         try:
+            language = request.form.get('language')
             audio = request.files.getlist('audio')
             if len(audio) <= 0:
                 return jsonify({'error' : 'lista de audio vazia.', 'solucao' : '', 'status' : 'ERROR' })
@@ -27,7 +28,10 @@ class SoundCaptcha(Resource):
                 filename = file.filename
                 destino = path.join(target, filename)
                 file.save(destino)
-            solucao = self.leitor_service.ler_audio(destino)
+            if language:
+                solucao = self.leitor_service.ler_audio(destino, language=language)
+            else
+                solucao = self.leitor_service.ler_audio(destino)
         except Exception as error:
             return jsonify({ 'error' : str(error), 'solucao': '', 'status': 'ERROR' })
         return jsonify({ 'solucao': solucao, 'status': 'SUCESSO' })
--- services/leitor_service.py
+++ services/leitor_service.py
@@ -1,14 +1,44 @@
 from os import path, remove
+import wave
+import sys
+
+
+import binascii
 import speech_recognition as sr
 
 
 class LeitorService():
-    def ler_audio(self, path_arq:str) -> str:
+    def ler_audio(self, path_arq:str, language:str='pt') -> str:
         if path.isfile(path_arq):
             rec = sr.Recognizer()
 
             with sr.AudioFile(path_arq) as source:
                 audio = rec.record(source)
-            solucao = rec.recognize_google(audio, language="pt")
+            solucao = rec.recognize_google(audio, language=language)
             remove(path_arq)
         return solucao.strip()
+    
+    def __limpar_ruidos(self, path_arq:str):
+        ip = wave.open('C:\\Users\\anagha\\Documents\\Python Scripts\\a1.wav', 'r')
+
+        op = wave.open('C:\\Users\\anagha\\Documents\\Python Scripts\\r_1.wav', 'w')
+        op.setparams(ip.getparams())
+
+        for i in range(ip.getnframes()):
+        iframes = ip.readframes(1)
+        amp = int(binascii.hexlify(iframes))
+        if amp > 32767:
+            amp = 65535 - int(binascii.hexlify(iframes))#-ve
+            print(amp)
+        else:
+            amp = int(binascii.hexlify(iframes))#+ve
+            print(amp)
+        if amp < 2000:
+            #make it zero
+            final_frame = '\x00\x00'
+        else:
+            #Keep the frame 
+            final_frame = iframe
+        op.writeframes(final_frame)
+        op.close()
+        ip.close()
